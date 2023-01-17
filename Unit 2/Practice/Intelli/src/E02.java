487058592c5864c37108fc0ee529591fcf64378b
Diego Ribas <diegoribas59@gmail.com>
2023-01-17 11:36:12 +0100

    update

---
+++ Unit 2/Practice/Intelli/src/E02.java
@@ -0,0 +1,10 @@
+/**
+ * Realiza una clase numero que almacene un numero entero y tenga las sogioemtes caracteristicas:
+ * Constructor por defecto que inicializa a 0 el numero interno.
+ * Constructor que inicializa el numero interno.
+ * Metodo aniade que permite sumarle un numero al valor interno.
+ * Metodo resta que resta
+ */
+
+public class E02 {
+}
