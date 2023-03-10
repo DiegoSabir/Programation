5bf35df378bca4ce54ebfa0b2d95e482038e04ca
Diego Ribas González <90776736+DiegoSabir@users.noreply.github.com>
2023-03-10 19:22:20 +0100

    update

--- Unit 9/E06/src/Main.java
+++ Unit 9/E06/src/Main.java
@@ -1,3 +1,11 @@
+/**
+ * Desarrolla un programa que lea constantemente desde la entrada estandar hasta recibir un enter como finalizador.
+ * Cada caracter sera almacenado de forma independiente en una coleccion que permitira recuperar el contenido en orden
+ * inverso al que fueron introducidos.
+ * Una vez recibido enter, recorre la coleccion mostrando por pantalla el contenido almacenado.
+ *
+ * Recuerda que los caracteres leerse por separado y no se debe emplear la clase String.
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
