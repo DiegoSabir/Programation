6af980cfc4cf30575593ba933da630a48d9fd779
Diego Ribas González <90776736+DiegoSabir@users.noreply.github.com>
2023-03-10 17:26:47 +0100

    Update

---
+++ Unit 9/E10/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Almacena en un HashMap los códigos postales de las provincias de Galicia.
+ *  - Muestra por pantalla los datos introducidos
+ *  - Pide un código postal y muestra la provincia asociada si existe sino avisa al usuario
+ *  - Elimina las provincias de Lugo y Pontevedra
+ *  - Muestra por pantalla los datos
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Unit 9/E11/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Programa que almacene y gestione los Nºexpediente y alumnos de la clase.
+ * Utiliza un HashMap para almacenar los datos de tus compañeros de clase.
+ *  - Indica cuantos alumnos hay
+ *  - Pide un Nº expediente, si existe muestra el nombre del alumno, sino indica el error
+ *  - Permite eliminar un alumno de nuestra lista
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
