2cc44a34ad80e315521e5c5b54103418bf307b6d
Diego Ribas González <90776736+DiegoSabir@users.noreply.github.com>
2023-03-17 17:05:36 +0100

    update

---
+++ Unit 9/E15/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Crea dos conjuntos ordenados de elementos Integer y realiza la siguiente:
+ * La suma, resta, multiplicacion de sus elementos. Crea un nuevo conjunto que contenga la union de los
+ * dos conjuntos iniciales
+ * Crea un nuevo conjunto que contenga la union de los dos conjuntos iniciales.
+ * Crea otro conjunto que contenga la diferencia de los dos conjuntos iniciales (eliminar los elementos de c2 en c1)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
